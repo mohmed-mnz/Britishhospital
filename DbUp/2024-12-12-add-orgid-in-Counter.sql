4ddd87f115fcb08ed60320a7579cd7fe42ee6d09
mohmed-mnz <mt1293@fayoum.edu.eg>
2024-12-12 00:14:22 +0200

    Add OrgId column to Counter table and update DbUp.csproj
    - Added `2024-12-12-add-orgid-in-Counter.sql` to `DbUp.csproj` as an embedded resource.
    - Updated SQL script to add `OrgId` column to `Counter` table if it doesn't exist.
    - Added foreign key constraint linking `OrgId` in `Counter` to `Id` in `Org`.
    - Removed any previous references to the old script.

---
+++ DbUp/2024-12-12-add-orgid-in-Counter.sql
@@ -0,0 +1,7 @@
+﻿if  not exists (select * from sys.columns where Name = N'OrgId' and Object_ID = Object_ID(N'Counters'))
+ begin
+	alter table Counter
+	add OrgId int  null;
+	alter table Counter
+	add constraint FK_Counter_Org foreign key (OrgId) references Org(Id) ;
+	end
