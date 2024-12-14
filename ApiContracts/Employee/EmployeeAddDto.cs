0e36ea6c0fe91ae32b6bdc9785147f32c25eb4ee
mohmed-mnz <mt1293@fayoum.edu.eg>
2024-12-14 11:22:33 +0200

    Add JSON serialization attributes to EmployeeAddDto
    - Added System.Text.Json.Serialization namespace.
    - Added properties to EmployeeAddDto with JsonPropertyName attributes.
    - Set MaxLength attribute for MobileNumber to 11.
    - Set Required and MaxLength attributes for Nid to 14.

--- ApiContracts/Employee/EmployeeAddDto.cs
+++ ApiContracts/Employee/EmployeeAddDto.cs
@@ -1,22 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ApiContracts.Employee;
 
 public class EmployeeAddDto
 {
+    [JsonPropertyName("userName")]
     public string Username { get; set; } = null!;
-
+    [JsonPropertyName("password")]
     public string Password { get; set; } = null!;
-
+    [JsonPropertyName("orgId")]
     public int? Orgid { get; set; }
-
+    [JsonPropertyName("email")]
     public string? Email { get; set; }
     [MaxLength(11)]
+    [JsonPropertyName("mobileNumber")]
     public string? MobileNumber { get; set; }
+    [JsonPropertyName("address")]
     public string? Address { get; set; }
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
     [Required]
     [MaxLength(14)]
+    [JsonPropertyName("nid")]
     public string? Nid { get; set; }
+    [JsonPropertyName("gender")]
     public bool? gender { get; set; }
 }
